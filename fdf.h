15fca442e85108deb30b92fd3e95a193b310b2ec
Geoffrey NEGRI <gnegri@e3r11p15.42.fr>
2017-12-06 11:08:28 +0100

    Changed structure + modified ft_reader

--- fdf.h
+++ fdf.h
@@ -14,48 +14,43 @@
 # define FDF_H
 
 # include <stdlib.h>
+# include "libft/libft.h"
 # include <unistd.h>
 # include "mlx.h"
 # include <fcntl.h>
 # include <sys/types.h>
 # include <sys/stat.h>
 # include <math.h>
-# include "libft/libft.h"
 
-# define WIN_WIDTH	1300
+# define WIN_WIDTH	1200
 # define WIN_HEIGHT	1200
-# define BUF_SIZE	1000
 
 # define BLUE 0x0041FF
 # define WHITE 0xFFFFFF
 # define GREEN 0x25FF50
 # define ORANGE 0x00FFA500
 
-typedef struct		s_point
+typedef struct		s_fdf
 {
-	int				x;
-	int				y;
-	char			z;
-	struct s_point	*next;
-}					t_point;
+	void			*mlx;
+	void			*win;
+	int				**tab;
+	int				nb_line;
+	int				nb_char;
+	int				length;
+	double			x;
+	double			y;
+	double			z;
+	double			x1;
+	double			x2;
+	double			y1;
+	double			y2;
+	int				def_zoom;
+	int				x_move;
+	int				y_move;
+}					t_fdf;
 
-typedef struct		s_img
-{
-	void			*pointeur_image;
-	int				*data;
-	int				size_l;
-	int				bpp;
-	int				endian;
-}					t_img;
-
-typedef struct		s_mlx
-{
-	void			*pointeur_mlx;
-	void			*pointeur_window;
-	t_img			img;
-}					t_mlx;
-
-char				**ft_reader(char *av);
-void				ft_draw(t_mlx mlx, t_point *point);
+int				ft_reader(int argc, char *argv, t_fdf *mlx);
+int				ft_atoi_fdf(const char *str, int *i);
 
 #endif
